d361d88f59ff4b6861c5c8629f4c29bca80d6f0f
Лавриненко Мария <maria.lavrinenko.work@gmail.com>
2016-06-18 19:53:20 +0300

    linking to already existed jquery

--- dist/js/tooltipster.bundle.js
+++ dist/js/tooltipster.bundle.js
@@ -5,17 +5,17 @@
  * MIT license
  */
 (function (root, factory) {
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module unless amdModuleId is set
-    define(["jQuery"], function (a0) {
-      return (factory(a0));
-    });
-  } else if (typeof exports === 'object') {
+  if (typeof exports === 'object') {
     // Node. Does not work with strict CommonJS, but
     // only CommonJS-like environments that support module.exports,
     // like Node.
-    module.exports = factory(require("jQuery"));
-  } else {
+    module.exports = factory($);
+  } else if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module unless amdModuleId is set
+		define(["jQuery"], function (a0) {
+			return (factory(a0));
+		});
+	} else {
     factory(jQuery);
   }
 }(this, function ($) {
